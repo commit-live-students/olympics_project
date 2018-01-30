da71f2e36d96507113e4564a6b287b096ee5d0ee
cchopade <cchopade@gmail.com>
2018-01-30 11:09:55 +0000

    Done

--- q03_split_country/build.py
+++ q03_split_country/build.py
@@ -5,6 +5,8 @@ from greyatomlib.olympics_project.q02_rename_columns.build import q02_rename_col
 
 
 def q03_summer_gold_medals(path):
-    "write your solution here"
     df = q02_rename_columns(path)
-   
+    df['country name'] = df['country name'].str.split('(',expand = True)
+    df.set_index('country name',inplace = True)
+    df.drop('Totals',inplace = True)
+    return df
