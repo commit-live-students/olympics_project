f98cc782a5e9444159d6bff74b1f58492dd98f51
viishnu712 <viishnu712@gmail.com>
2018-02-08 06:14:52 +0000

    Done

--- q04_country_with_most_gold_medals/build.py
+++ q04_country_with_most_gold_medals/build.py
@@ -5,7 +5,9 @@ from greyatomlib.olympics_project.q03_split_country.build import q03_summer_gold
 
 
 def q04_country_with_most_gold_medals(path):
-    "write your solution here"
     df = q03_summer_gold_medals(path)
-    
-
+    dfnew = df['Gold']
+    total_column = (dfnew.iloc[:,2:3])
+    top_max = total_column.loc[df['Gold'].idxmax()]
+    top =(top_max).index[0].replace("\xc2\xa0", "")
+    return top
