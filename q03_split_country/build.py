dc6757e2b2bad6455d09b3310cece3362f09cd96
Srishtisawla <sawlasrishti@gmail.com>
2018-02-10 18:33:13 +0000

    Done

--- q03_split_country/build.py
+++ q03_split_country/build.py
@@ -3,12 +3,11 @@ import numpy as np
 from sklearn.model_selection import train_test_split
 from greyatomlib.olympics_project.q02_rename_columns.build import q02_rename_columns
 
-path = "data/olympics.csv"
+#path = "data/olympics.csv"
 
-#print list(df.columns.values)
 def q03_summer_gold_medals(path):
     df = q02_rename_columns(path)
     df['country name'],df['country code']=df['country name'].str.split('(',1).str
     df.set_index('country name',inplace=True)
-    del df['country code']
-    return df[0:146]
+    del df['Combined total']
+    return df[1:147]
