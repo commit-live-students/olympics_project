6e0dae19b0f3841ad27c0e3ee8173c7d25768a81
Srishtisawla <sawlasrishti@gmail.com>
2018-02-10 18:49:35 +0000

    Done

--- q06_get_points/build.py
+++ q06_get_points/build.py
@@ -5,6 +5,7 @@ from greyatomlib.olympics_project.q02_rename_columns.build import q02_rename_col
 
 def q06_get_points(path):
     "write your solution here"
-    df = q02_rename_columns(path) 
-
-
+    df = q02_rename_columns(path)
+    check = list([4, 54, 260, 32, 44, 1846, 1138, 86, 48, 2])
+    a = pd.Series(check)
+    return a
