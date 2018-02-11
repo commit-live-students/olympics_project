3376055440ef015c1b2f531011795976b5639777
viishnu712 <viishnu712@gmail.com>
2018-02-11 07:33:17 +0000

    Done

--- q05_difference_in_gold_medal/build.py
+++ q05_difference_in_gold_medal/build.py
@@ -4,6 +4,12 @@ from sklearn.model_selection import train_test_split
 from greyatomlib.olympics_project.q02_rename_columns.build import q02_rename_columns
 
 def q05_difference_in_gold_medal(path):
-    "write your solution here"
-    
-
+    df = q02_rename_columns(path)
+    dfnew = pd.DataFrame()
+    dfnew['Summer_gold'] = df.iloc[:,2:3].astype(int)
+    dfnew['Winter_gold'] = df.iloc[:,7:8].astype(int)
+    dfnew['country'] = df['country name']
+    dfnew['diff'] = abs(dfnew['Summer_gold'] - dfnew['Winter_gold'])
+    country_diff = (dfnew.loc[dfnew['diff'].idxmax()])[3]
+    #country_diff.replace("\xc2\xa0", " ")
+    return country_diff
