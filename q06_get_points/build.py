a495568213888a084df87acf8cd09d8cd8ab3fc9
viishnu712 <viishnu712@gmail.com>
2018-02-14 11:38:41 +0000

    Done

--- q06_get_points/build.py
+++ q06_get_points/build.py
@@ -4,7 +4,11 @@ from sklearn.model_selection import train_test_split
 from greyatomlib.olympics_project.q02_rename_columns.build import q02_rename_columns
 
 def q06_get_points(path):
-    "write your solution here"
-    df = q02_rename_columns(path) 
-
-
+    df = q02_rename_columns(path)
+    df.iloc[:,12:15] = df.iloc[:,12:15].apply(pd.to_numeric)
+    df['Points1'] = (df.iloc[:,12:13] * 3)
+    df['Points2'] = (df.iloc[:,13:14] * 2)
+    df['Points3'] = (df.iloc[:,14:15] * 1)
+    df['Points'] = df['Points1']+df['Points2']+df['Points3']
+    df['Points'] = df['Points']*2
+    return (pd.Series(df['Points'])) 
