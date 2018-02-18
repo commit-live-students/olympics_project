9ffd79c388e0312d527f96aaecabf60778e04965
vp999 <patilvish@gmail.com>
2018-02-18 17:13:57 +0000

    Done

--- q02_rename_columns/build.py
+++ q02_rename_columns/build.py
@@ -6,4 +6,8 @@ from greyatomlib.olympics_project.q01_load_data.build import q01_load_data
 def q02_rename_columns(path):
     "write your solution here"
     df = q01_load_data(path)
-    
+    return df.rename(columns={ '01 !':'Gold','02 !':'Silver','03 !': 'Bronze',
+                               '01 !.1':'Gold','02 !.1':'Silver','03 !.1': 'Bronze',
+                               '01 !.2':'Gold','02 !.2':'Silver','03 !.2': 'Bronze',
+                               'Total.1':'Total',
+                             })
