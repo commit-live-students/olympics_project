90cc9d420ca69d8d6f72dd3d25099f0b18742df5
vp999 <patilvish@gmail.com>
2018-02-18 19:53:30 +0000

    Done

--- q05_difference_in_gold_medal/build.py
+++ q05_difference_in_gold_medal/build.py
@@ -5,5 +5,12 @@ from greyatomlib.olympics_project.q02_rename_columns.build import q02_rename_col
 
 def q05_difference_in_gold_medal(path):
     "write your solution here"
-    
+    df = q02_rename_columns(path)
+    df=df[:-1]
+    #golds= df.iloc[:,[2,7]]
+    gold_diff= df.iloc[:,2].astype(int) - df.iloc[:,7].astype(int)
+    return gold_diff.max()
 
+#path = "data/olympics.csv"
+#df = q05_difference_in_gold_medal(path)
+#print(df)
