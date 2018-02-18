bf7cc35a76b102d4ac206c6240671b95d84ae7b2
vp999 <patilvish@gmail.com>
2018-02-18 16:46:08 +0000

    Done

--- q01_load_data/build.py
+++ q01_load_data/build.py
@@ -1,15 +1,10 @@
 import pandas as pd
 import numpy as np
 from sklearn.model_selection import train_test_split
+import pep8
 
 
 def q01_load_data(path):
-    "write your solution here"
-    # use .read_csv function to read the
-    # data and header=0 to skip the first row
-    df = pd.read_csv(path, header=0)
-    new_header = df.iloc[0]  # grab the first row for the header
-    new_header[0] = 'country name'
-    df = df[1:]  # take the data less the header row
-    df.columns = new_header  # set the header row as the df header
+    df = pd.read_csv(path, header=0, skiprows=1)
+    df = df.rename(columns={'Unnamed: 0': 'country name', })
     return df
