4f074e63d71a863a6a2b5719373d6bc41f9217bd
brijesh-upadhyay <brijesh_upadhyay@ymail.com>
2018-02-28 05:29:42 +0000

    Done

--- q01_load_data/build.py
+++ q01_load_data/build.py
@@ -1,6 +1,9 @@
+# %load q01_load_data/build.py
 import pandas as pd
 import numpy as np
 from sklearn.model_selection import train_test_split
+
+path = 'data/olympics.csv'
 
 
 def q01_load_data(path):
