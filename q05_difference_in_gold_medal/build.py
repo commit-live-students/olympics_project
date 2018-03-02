93aacdc2843330ae4166d93c660058f08eb52168
djocz <jocelynd789@gmail.com>
2018-03-02 04:16:52 +0000

    Done

--- q05_difference_in_gold_medal/build.py
+++ q05_difference_in_gold_medal/build.py
@@ -5,5 +5,6 @@ from greyatomlib.olympics_project.q02_rename_columns.build import q02_rename_col
 
 def q05_difference_in_gold_medal(path):
     "write your solution here"
-    
-
+    df = q02_rename_columns(path)
+    diff = df['Gold'].iloc[:-1,0].astype('int64') - df['Gold'].iloc[:-1,1].astype('int64')
+    return diff.max()
