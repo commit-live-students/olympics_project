9e237be0dab6826dc36527c11a7345257b9ab42e
bhushan45 <bhushanshewale45@gmail.com>
2018-03-04 10:17:27 +0000

    Done

--- q03_split_country/build.py
+++ q03_split_country/build.py
@@ -7,4 +7,10 @@ from greyatomlib.olympics_project.q02_rename_columns.build import q02_rename_col
 def q03_summer_gold_medals(path):
     "write your solution here"
     df = q02_rename_columns(path)
-   
+    country_cols=pd.DataFrame (
+                        df['country name'].str.split('(',1).tolist()
+                        , columns=['country name','country code'])
+    df= df.drop(['country name'], axis=1)
+    df.loc[:,'country name']=country_cols['country name']
+    df = df.set_index(keys=['country name'])
+    return df[:-1]
