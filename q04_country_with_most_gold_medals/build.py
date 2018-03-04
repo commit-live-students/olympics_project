c3ece2deacfe7c018f5b27c29496e163318d7bd0
bhushan45 <bhushanshewale45@gmail.com>
2018-03-04 10:19:33 +0000

    Done

--- q04_country_with_most_gold_medals/build.py
+++ q04_country_with_most_gold_medals/build.py
@@ -7,5 +7,4 @@ from greyatomlib.olympics_project.q03_split_country.build import q03_summer_gold
 def q04_country_with_most_gold_medals(path):
     "write your solution here"
     df = q03_summer_gold_medals(path)
-    
-
+    return df.iloc[:,2].idxmax()
