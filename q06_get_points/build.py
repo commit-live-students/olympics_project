72a14ae4440c0d134b040ad04103945a3414a0ec
bhushan45 <bhushanshewale45@gmail.com>
2018-03-04 10:30:08 +0000

    Done

--- q06_get_points/build.py
+++ q06_get_points/build.py
@@ -5,6 +5,10 @@ from greyatomlib.olympics_project.q02_rename_columns.build import q02_rename_col
 
 def q06_get_points(path):
     "write your solution here"
-    df = q02_rename_columns(path) 
+    df = q02_rename_columns(path)
+    df['Gold']= df['Gold'].astype(int)
+    df['Silver']= df['Silver'].astype(int)
+    df['Bronze']= df['Bronze'].astype(int)
 
-
+    df['points']=np.sum( df['Gold']  * 3  , axis=1) +   np.sum( df['Silver'] * 2  , axis=1) +   np.sum( df['Bronze'] *1    , axis=1)
+    return df['points']
