052a716e9d9a5a29ee0f179f34aa31198c3629b8
ms10031990 <ms10031990@yahoo.com>
2018-03-04 15:11:05 +0000

    Done

--- q04_country_with_most_gold_medals/build.py
+++ q04_country_with_most_gold_medals/build.py
@@ -1,3 +1,4 @@
+# %load q04_country_with_most_gold_medals/build.py
 import pandas as pd
 import numpy as np
 from sklearn.model_selection import train_test_split
@@ -5,9 +6,15 @@ from greyatomlib.olympics_project.q03_split_country.build import q03_summer_gold
 
 
 def q04_country_with_most_gold_medals(path):
-    "write your solution here"
+    'write your solution here'
     df = q03_summer_gold_medals(path)
-
-    df['Max_Gold']= pd.DataFrame(df.iloc[:,1].astype('int64')) + pd.DataFrame(df.iloc[:,6].astype('int64')) + pd.DataFrame(df.iloc[:,11].astype('int64'))
-
+    
+    df['Max_Gold'] = pd.DataFrame(df.iloc[:,1].astype('int64')) + pd.DataFrame(df.iloc[:,6].astype('int64'))
+    
     return df['Max_Gold'].idxmax()
+           
+    
+q04_country_with_most_gold_medals('./data/olympics.csv')
+
+
+
--- q05_difference_in_gold_medal/build.py
+++ q05_difference_in_gold_medal/build.py
@@ -1,9 +1,26 @@
+# %load q05_difference_in_gold_medal/build.py
 import pandas as pd
 import numpy as np
 from sklearn.model_selection import train_test_split
 from greyatomlib.olympics_project.q02_rename_columns.build import q02_rename_columns
+from unittest import TestCase
+from inspect import getargspec
 
 def q05_difference_in_gold_medal(path):
-    "write your solution here"
+    'write your solution here'
     
+    df = q02_rename_columns(path)
+    df=df[:-1] #Drop last row which is total of all individual columns
+
+    df['diff_max'] = pd.DataFrame(df.iloc[:,2].astype('int64')) - pd.DataFrame(df.iloc[:,7].astype('int64'))
+    return df['diff_max'].max()
+                         
+
+arg = getargspec(q05_difference_in_gold_medal).args
+print len(arg)
+df = q05_difference_in_gold_medal('./data/olympics.csv')
+print df
+    
+
+
 
