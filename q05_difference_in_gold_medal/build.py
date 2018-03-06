7f8dd1bd487fd699ce15e5e05ebb9cb63ee8047b
Sharad30 <sharad30.ss@gmail.com>
2018-03-06 18:18:44 +0000

    Done

--- q05_difference_in_gold_medal/build.py
+++ q05_difference_in_gold_medal/build.py
@@ -2,8 +2,16 @@ import pandas as pd
 import numpy as np
 from sklearn.model_selection import train_test_split
 from greyatomlib.olympics_project.q02_rename_columns.build import q02_rename_columns
+from greyatomlib.olympics_project.q03_split_country.build import q03_summer_gold_medals
 
 def q05_difference_in_gold_medal(path):
-    "write your solution here"
-    
 
+    data = q03_summer_gold_medals(path)
+    gold_data = data.Gold
+    diff = []
+
+    for i in gold_data.values:
+
+        diff.append(abs(int(i[0]) - int(i[1])))
+
+    return max(diff)
