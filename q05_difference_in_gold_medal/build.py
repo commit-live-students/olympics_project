5ac7e1fb1be3737ea13b10f3f9e7790fafe9d906
PBPatil <theprasadpatil@gmail.com>
2018-03-07 09:49:35 +0000

    Done

--- q05_difference_in_gold_medal/build.py
+++ q05_difference_in_gold_medal/build.py
@@ -2,8 +2,8 @@ import pandas as pd
 import numpy as np
 from sklearn.model_selection import train_test_split
 from greyatomlib.olympics_project.q02_rename_columns.build import q02_rename_columns
-
+path="./data/olympics.csv"
 def q05_difference_in_gold_medal(path):
+    df = q02_rename_columns(path)
     "write your solution here"
-    
-
+    return max(abs(df.iloc[:-1,2].astype(int) - df.iloc[:-1,7].astype(int)))
