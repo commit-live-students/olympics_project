1d1eb6bd298f59ed91b900707e2aed19db14a7b7
sohelahmadkhan <sohelahmadkhan000@gmail.com>
2018-03-08 20:44:20 +0000

    Done

--- q03_split_country/build.py
+++ q03_split_country/build.py
@@ -5,16 +5,22 @@ from sklearn.model_selection import train_test_split
 from greyatomlib.olympics_project.q02_rename_columns.build import q02_rename_columns
 
 
-def q03_summer_gold_medals(path):
-    'write your solution here'
-    df = q02_rename_columns(path)
-    cname = df.loc[:,'country name']
-    for i in range(1,cname.shape[0]):
-        cname[i] = cname[i].split('(')[0].rstrip()
+# def q03_summer_gold_medals(path):
+#     'write your solution here'
+#     df = q02_rename_columns(path)
+#     cname = df.loc[:,'country name']
+#     for i in range(1,cname.shape[0]):
+#         cname[i] = cname[i].split('(')[0].rstrip()
 
-    df1 = df.drop(labels='country name',axis=1)
-    df1.index = cname
-    df2 = df1.drop(labels='Totals',axis=0)
-    return df2
+#     df1 = df.drop(labels='country name',axis=1)
+#     df1.index = cname
+#     df2 = df1.drop(labels='Totals',axis=0)
+#     return df2
+def q03_summer_gold_medals(path):
+    df = q02_rename_columns(path)
+    df['country name'],df['country code']=df['country name'].str.split('(',1).str
+    df.set_index('country name',inplace=True)
+    del df['Combined total']
+    return df[1:147]
    
 
--- q04_country_with_most_gold_medals/build.py
+++ q04_country_with_most_gold_medals/build.py
@@ -1,11 +1,23 @@
+# %load q04_country_with_most_gold_medals/build.py
 import pandas as pd
 import numpy as np
 from sklearn.model_selection import train_test_split
 from greyatomlib.olympics_project.q03_split_country.build import q03_summer_gold_medals
 
-
+#path='./data/olympics.csv'
 def q04_country_with_most_gold_medals(path):
-    "write your solution here"
+    'write your solution here'
     df = q03_summer_gold_medals(path)
+    gold_count = df.iloc[:,1].sort_values(ascending=False)
+
+    df['gold_count'] = gold_count
+    max = gold_count.max()
+    a = df[df['gold_count'] == max]
+    aa = a.index
+    aaa = aa[0]
+    return aaa
+#q04_country_with_most_gold_medals(path)
     
+
+
 
