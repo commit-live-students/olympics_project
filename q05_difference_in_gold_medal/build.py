7795a3f01d88dbec88f25b4789df5df65da35b17
Alex19427 <alex19427@gmail.com>
2018-03-17 12:03:49 +0530

    update

--- q05_difference_in_gold_medal/build.py
+++ q05_difference_in_gold_medal/build.py
@@ -3,7 +3,7 @@ import numpy as np
 from sklearn.model_selection import train_test_split
 from greyatomlib.olympics_project.q02_rename_columns.build import q02_rename_columns
 
-def q05_difference_in_gold_medal(path):
+def q05_difference_in_gold_medal():
     "write your solution here"
     
 
--- q06_get_points/build.py
+++ q06_get_points/build.py
@@ -5,17 +5,5 @@ from greyatomlib.olympics_project.q02_rename_columns.build import q02_rename_col
 
 path = "data/olympics.csv"
 
-def q06_get_points(path):
-    "write your solution here"
-    df = q02_rename_columns(path)
-    df['Gold Total'] = df[['Gold']].astype(int).sum(1).apply(lambda x: x * 3) # create a column for Gold total
-    df['Silver Total'] = df[['Silver']].astype(int).sum(1).apply(lambda x: x * 2)
-    df['Bronze Total'] = df[['Bronze']].astype(int).sum(1).apply(lambda x: x * 1)
-    df['Points'] = df['Gold Total'].astype(int) + df['Silver Total'].astype(int) + df['Bronze Total'].astype(int)
-    df = df.drop(['Gold Total', 'Silver Total', 'Bronze Total'], axis=1)
-    return df[['Points']]
 
-
-
-print(list(q06_get_points(path)[:10].values))
 
