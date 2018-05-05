d409e3cffbe19ec7b8c4f29c797b04deba151438
bharadwajk9 <utkb.k9@gmail.com>
2018-05-05 04:46:10 +0000

    Done

--- q02_rename_columns/build.py
+++ q02_rename_columns/build.py
@@ -1,9 +1,11 @@
 import pandas as pd
-import numpy as np
-from sklearn.model_selection import train_test_split
+import numpy
 from greyatomlib.olympics_project.q01_load_data.build import q01_load_data
 
+path = './data/olympics.csv'
 def q02_rename_columns(path):
-    "write your solution here"
     df = q01_load_data(path)
-    
+    df = df.rename(columns={'01 !': 'Gold', '02 !':'Silver', '03 !':'Bronze'})
+    return df
+#q02_rename_columns(path)
+
