9f29bdbf70570306c53d6800edfe311728a043cc
HardikaB <hardi.2607@gmail.com>
2018-05-05 15:54:15 +0000

    Done

--- q04_country_with_most_gold_medals/build.py
+++ q04_country_with_most_gold_medals/build.py
@@ -1,3 +1,4 @@
+# %load q04_country_with_most_gold_medals/build.py
 import pandas as pd
 import numpy as np
 from sklearn.model_selection import train_test_split
@@ -5,7 +6,11 @@ from greyatomlib.olympics_project.q03_split_country.build import q03_summer_gold
 
 
 def q04_country_with_most_gold_medals(path):
-    "write your solution here"
+    'write your solution here'
     df = q03_summer_gold_medals(path)
+    df_g=df.iloc[:,11].sort_values(ascending=False).reset_index().iloc[0,0]
+    return df_g
     
+
+
 
