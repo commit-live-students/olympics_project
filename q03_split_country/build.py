b7e8ad74d0d3a14e2746c97c0ec006618b053087
rushi0109 <rushi0109@gmail.com>
2018-05-05 15:28:05 +0000

    Done

--- q03_split_country/build.py
+++ q03_split_country/build.py
@@ -8,23 +8,15 @@ from greyatomlib.olympics_project.q02_rename_columns.build import q02_rename_col
 def q03_summer_gold_medals(path):
     'write your solution here'
     df = q02_rename_columns(path)
+    
+    df['country name'] = df['country name'].str.split('(',1).apply(lambda x :x[0])
+    df.set_index('country name',inplace = True)
+    df.dropna(inplace = True)
+    df.drop(['Totals'],axis=0,inplace = True)
     print(df.shape)
     print(df.columns)
-    dfNew = pd.DataFrame(df['country name'].str.split('(',1).tolist(),
-                                   columns = ['country name New','country code'])
-    dfNew['country code'] = dfNew['country code'].astype('str')
-    print(dfNew['country code'].dtype)
-    countryCodeDF = pd.DataFrame(dfNew['country code'].str.split(')',1).tolist(),
-                                   columns = ['country code Final','trash']) 
-    mergedDF = df.join(dfNew)
-    mergedDF = mergedDF.join(countryCodeDF)
-    finalDF = mergedDF.drop(['country code','trash','country name','Combined total'],axis=1)
-    finalDF = finalDF.rename(columns={'country name New': 'country name', 'country code Final': 'country code'})
-    finalDF = finalDF.set_index('country name')
-    print(finalDF.shape)
-    print(finalDF.columns)
-    finalDF = finalDF.dropna()
-    return finalDF
+    
+    return df
 
 
 q03_summer_gold_medals('./data/olympics.csv')     
