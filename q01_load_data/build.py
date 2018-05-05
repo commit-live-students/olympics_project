850b993dc07df36fd9e70492829ca493bb61a8d4
navazsayani <navaz.sayani@somaiya.edu>
2018-05-05 04:37:44 +0000

    Done

--- q01_load_data/build.py
+++ q01_load_data/build.py
@@ -1,10 +1,16 @@
+# %load q01_load_data/build.py
 import pandas as pd
 import numpy as np
 from sklearn.model_selection import train_test_split
 
 
 def q01_load_data(path):
-    "write your solution here"
-    # use .read_csv function to read the
-    # data and header=0 to skip the first row
+    df = pd.read_csv(path,header = 0)
+    df.loc[0][0] = 'country name'
+    df.columns = df.loc[0]
+    df = df.drop(0)
+    return (df)
+
+q01_load_data('./data/olympics.csv')
+
 
