a5105f37cfc9e3ac5baf4470fbe9e8f226b31c72
chirag-shinde <shinde.chirag2204@gmail.com>
2018-05-05 14:58:11 +0000

    Done

--- q03_split_country/build.py
+++ q03_split_country/build.py
@@ -1,3 +1,4 @@
+# %load q03_split_country/build.py
 import pandas as pd
 import numpy as np
 from sklearn.model_selection import train_test_split
@@ -5,6 +6,11 @@ from greyatomlib.olympics_project.q02_rename_columns.build import q02_rename_col
 
 
 def q03_summer_gold_medals(path):
-    "write your solution here"
+    'write your solution here'
     df = q02_rename_columns(path)
-   
+    country_name = df['country name'].str.split().apply(lambda x: x[0])
+    df.rename(index=country_name,inplace=True)
+    df.drop(['country name'], axis=1,inplace=True)
+    return df[:-1]
+q03_summer_gold_medals('./data/olympics.csv')
+
