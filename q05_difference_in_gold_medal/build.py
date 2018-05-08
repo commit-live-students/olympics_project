542d25e9172ff8ec07cd064443e3d8b46004f96f
navazsayani <navaz.sayani@somaiya.edu>
2018-05-08 09:20:24 +0000

    Done

--- q05_difference_in_gold_medal/build.py
+++ q05_difference_in_gold_medal/build.py
@@ -1,9 +1,18 @@
+# %load q05_difference_in_gold_medal/build.py
 import pandas as pd
 import numpy as np
 from sklearn.model_selection import train_test_split
-from greyatomlib.olympics_project.q02_rename_columns.build import q02_rename_columns
+from greyatomlib.olympics_project.q03_split_country.build import q03_summer_gold_medals
 
-def q05_difference_in_gold_medal():
-    "write your solution here"
+path = './data/olympics.csv'
+def q05_difference_in_gold_medal(path):
+    df = q03_summer_gold_medals(path)
+    return (df.iloc[:,1].astype(np.int) - df.iloc[:,6].astype(np.int)).max()
     
+    #   'write your solution here'
+    
+
+q05_difference_in_gold_medal(path)
+
+
 
