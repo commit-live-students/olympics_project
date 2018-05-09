4a86dfe09f5103d4907643c78b65fc1ada77d14d
bharadwajk9 <utkb.k9@gmail.com>
2018-05-09 15:44:00 +0000

    Done

--- q06_get_points/build.py
+++ q06_get_points/build.py
@@ -1,9 +1,17 @@
 import pandas as pd
 import numpy as np
-from sklearn.model_selection import train_test_split
 from greyatomlib.olympics_project.q02_rename_columns.build import q02_rename_columns
 
-path = "data/olympics.csv"
+path = './data/olympics.csv'
+def q06_get_points(path):
+    df = q02_rename_columns(path)
+    wt = [3,2,1]
+    df1 = df.iloc[:,[12,13,14]].astype(int)
+    res = df1.mul(wt)
+    df['Points'] = res.sum(axis=1)
+    return res.sum(axis=1) 
+
+q06_get_points(path)
 
 
 
