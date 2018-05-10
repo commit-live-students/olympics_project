a03d0b92b457a6416e099a03df53fa264d44edb2
chirag-shinde <shinde.chirag2204@gmail.com>
2018-05-10 15:38:20 +0000

    Done

--- q05_difference_in_gold_medal/build.py
+++ q05_difference_in_gold_medal/build.py
@@ -1,9 +1,14 @@
+# %load q05_difference_in_gold_medal/build.py
 import pandas as pd
 import numpy as np
 from sklearn.model_selection import train_test_split
 from greyatomlib.olympics_project.q02_rename_columns.build import q02_rename_columns
 
-def q05_difference_in_gold_medal():
-    "write your solution here"
-    
+def q05_difference_in_gold_medal(path):
+    df = q02_rename_columns('./data/olympics.csv')
+    df.rename(index=df['country name'],inplace=True)
+    df = df[:-1]
+    return (df.iloc[:,2].astype(np.int) - df.iloc[:,7].astype(np.int)).max()
+q05_difference_in_gold_medal('./data/olympics.csv')
+
 
