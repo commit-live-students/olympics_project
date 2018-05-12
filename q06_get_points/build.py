a247cc154047c3d8640d47362edb8b9a2b3cf237
chirag-shinde <shinde.chirag2204@gmail.com>
2018-05-12 05:07:36 +0000

    Done

--- q06_get_points/build.py
+++ q06_get_points/build.py
@@ -1,9 +1,13 @@
+# %load q06_get_points/build.py
 import pandas as pd
 import numpy as np
 from sklearn.model_selection import train_test_split
 from greyatomlib.olympics_project.q02_rename_columns.build import q02_rename_columns
 
-path = "data/olympics.csv"
+path = 'data/olympics.csv'
 
+def q06_get_points(path):
+    df = q02_rename_columns(path)
+    return df.iloc[:,-4].astype(np.int) * 3 + df.iloc[:,-3].astype(np.int) * 2 + df.iloc[:,-2].astype(np.int)
 
 
