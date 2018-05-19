14dc4af03542d6ed0f694edb54f7a050a567be0e
siddesh001 <siddesh.001@gmail.com>
2018-05-19 09:09:24 +0000

    Done

--- q04_country_with_most_gold_medals/build.py
+++ q04_country_with_most_gold_medals/build.py
@@ -1,3 +1,4 @@
+# %load q04_country_with_most_gold_medals/build.py
 import pandas as pd
 import numpy as np
 from sklearn.model_selection import train_test_split
@@ -5,7 +6,12 @@ from greyatomlib.olympics_project.q03_split_country.build import q03_summer_gold
 
 
 def q04_country_with_most_gold_medals(path):
-    "write your solution here"
+    'write your solution here'
     df = q03_summer_gold_medals(path)
     
+    a=pd.DataFrame(((df[:]['Gold']).values).astype(np.int),index=df['# Summer'].index)
+    max_country=(a[2].argmax())
+    #print(a[a.index==max_country])
+    return str(max_country)
+
 
