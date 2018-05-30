7c370548ad9b525917ed167adcf167be727d64c0
SL345 <sherilyn345@gmail.com>
2018-05-30 15:07:59 +0000

    Done

--- q02_rename_columns/build.py
+++ q02_rename_columns/build.py
@@ -1,9 +1,14 @@
+# %load q02_rename_columns/build.py
 import pandas as pd
 import numpy as np
 from sklearn.model_selection import train_test_split
 from greyatomlib.olympics_project.q01_load_data.build import q01_load_data
 
 def q02_rename_columns(path):
-    "write your solution here"
+    'write your solution here'
     df = q01_load_data(path)
+    df.columns = ['country name', '# Summer', 'Gold', 'Silver', 'Bronze', 'Total', '# Winter', 'Gold', 'Silver', 'Bronze',
+         'Total', '# Games', 'Gold', 'Silver', 'Bronze', 'Combined total']
+    return df
     
+
