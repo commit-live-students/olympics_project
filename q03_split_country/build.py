9c235489f701857859b6c3d53e94dcdc53d15e2e
gauravpednekar <grv.pednekar@gmail.com>
2018-06-22 06:29:30 +0000

    Done

--- q03_split_country/build.py
+++ q03_split_country/build.py
@@ -1,3 +1,4 @@
+# %load q03_split_country/build.py
 import pandas as pd
 import numpy as np
 from sklearn.model_selection import train_test_split
@@ -5,6 +6,18 @@ from greyatomlib.olympics_project.q02_rename_columns.build import q02_rename_col
 
 
 def q03_summer_gold_medals(path):
-    "write your solution here"
+    'write your solution here'
     df = q02_rename_columns(path)
+    count = df['country name'].str.split('(')
+#     print(count)
+    val = count.apply(lambda x :x[0])
+#     return val
+    df['country name'] = val
+    df.set_index('country name',inplace = True)
+    df.dropna(inplace = True)
+    df.drop(['Totals'],axis=0,inplace = True)
+    return df
+
+q03_summer_gold_medals('./data/olympics.csv')
    
+
