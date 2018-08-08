d97613daf3f3b685d5f7adeaee97090e36e5119c
sonal7930 <sonal.chandra7930@gmail.com>
2018-08-08 10:32:27 +0000

    Done

--- q03_split_country/build.py
+++ q03_split_country/build.py
@@ -1,10 +1,25 @@
+# %load q03_split_country/build.py
 import pandas as pd
 import numpy as np
 from sklearn.model_selection import train_test_split
 from greyatomlib.olympics_project.q02_rename_columns.build import q02_rename_columns
 
+path = 'data/olympics.csv'
 
 def q03_summer_gold_medals(path):
-    "write your solution here"
+    'write your solution here'
     df = q02_rename_columns(path)
+    cname = df.loc[:,'country name']
+    
+    for i in range(1,cname.shape[0]):
+        cname[i] = cname[i].split('(')[0].rstrip() #Split the characters with '(' and strip the unnecessary characters using rstrip()
+    
+    df1 = df.drop(labels='country name',axis=1)
+    df1.index = cname
+    df2 = df1.drop(labels='Totals',axis=0)
+    
+    return df2
+
+#print(q03_summer_gold_medals(path))
    
+
