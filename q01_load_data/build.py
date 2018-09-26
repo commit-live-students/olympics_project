75a9bbe2e4d42c8676978aa5fd092643b0367c0b
amol120586 <amol.24365@gmail.com>
2018-09-26 06:21:45 +0000

    Done

--- q01_load_data/build.py
+++ q01_load_data/build.py
@@ -1,10 +1,20 @@
+# %load q01_load_data/build.py
 import pandas as pd
 import numpy as np
 from sklearn.model_selection import train_test_split
 
 
 def q01_load_data(path):
-    "write your solution here"
+    'write your solution here'
     # use .read_csv function to read the
     # data and header=0 to skip the first row
+    df = pd.read_csv(path, header=0)
+    header = df.iloc[0]
+    header[0] = 'country name'
+    df = df.iloc[1:,:]
+    df.columns = header
+    return df
+    
+
+
 
